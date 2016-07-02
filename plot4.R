edabd5b0db20c87d90d308b063af3999b135c13a
crbeeman <crbeeman@users.noreply.github.com>
2016-07-02 13:49:40 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(lubridate)
+
 data4 <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
 data4 <- data4 %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),
                V3 = as.numeric(as.character(V3)),
